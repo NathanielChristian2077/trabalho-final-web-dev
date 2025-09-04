65c41b5dc7e765c402322511e5879ef146e69ced
ContaIFSC <alunolages@lages.sc>
2025-09-04 09:19:29 -0300

    setup.sh frontend+backend

--- setup.sh
+++ setup.sh
@@ -1,23 +1,38 @@
 set -e
 
-echo "== Uploading db trough Docker =="
-docker compose -f infra/docker-compose.yml up -d
+if [-f .env]; then
+  echo "== Uploading db trough Docker =="
+  docker compose -f infra/docker-compose.yml up -d
 
-echo "== Installing backend dependencies =="
-cd backend
-npm install
+  echo "== Installing backend dependencies =="
+  cd backend
+  npm install
 
-echo "== Prisma generate =="
-npx prisma generate
+  echo "== Prisma generate =="
+  npx prisma generate
 
-echo "== Prisma migrate dev =="
-npx prisma migrate dev --name init
+  echo "== Prisma migrate dev =="
+  npx prisma migrate dev --name init
 
-echo "== Starting seed =="
-if [ -f prisma/seed.ts ]; then
-  npm run db:seed
-else
-  echo "Seed not found, skipping..."
-fi
+  echo "== Starting seed =="
+  if [ -f prisma/seed.ts ]; then
+    npm run db:seed
+  else
+    echo "Seed not found, skipping..."
+  fi
 
-echo "== Setup succeded! run 'npm run dev' on /backend. =="
+  echo "== Installing frontend dependencies =="
+  cd ../frontend
+  npm install
+  npm install i -D @tailwindcss/vite @tailwindcss/cli tailwindcss postcss autoprefixer
+
+  echo "== Installing libs =="
+  npm i react-router-dom zustand axios d3
+  npm i -D @types/d3 @types/react-router-dom
+
+  echo "== Installing utils (optional:recommended) =="
+  npm i -D eslint prettier vite-tsconfig-paths
+
+  echo "== Setup succeded! run 'npm run dev' on /backend or /frontend. =="
+else 
+  echo "Warning: set '.env' before running setup.sh"
