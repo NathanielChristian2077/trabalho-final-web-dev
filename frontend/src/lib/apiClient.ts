e24e239104dffa4e2b2600baf4fea6fcf5b9dabd
NinIF <nathas10.alves@gmail.com>
2025-11-06 09:41:54 -0300

    Remaking frontend

--- frontend/src/lib/apiClient.ts
+++ frontend/src/lib/apiClient.ts
@@ -1,22 +1,23 @@
-import axios from 'axios'
+import axios from 'axios';
+import { useAuthStore } from '../stores/useAuthStore';
 import { env } from './env'
 
 
 const api = axios.create({ baseURL: env.apiUrl });
 
 api.interceptors.request.use((config) => {
-    const token = localStorage.getItem("token")
-    if (token) config.headers.Authorization = `Bearer ${token}`
-    return config
+  const token = localStorage.getItem('accessToken');
+  if (token) config.headers.Authorization = `Bearer ${token}`
+  return config
 });
 
-api.interceptors.response.use(r => r, err => {
-    const code = err?.response?.status;
-    if (code === 401 || code === 403) {
-        localStorage.removeItem("token");
-        window.location.href = "/login";
-    }
-    return Promise.reject(err);
+api.interceptors.response.use((res) => res, err => {
+  const code = err?.response?.status;
+  if (code === 401 || code === 403) {
+    useAuthStore.getState().logout();
+    window.location.href = "/login";
+  }
+  return Promise.reject(err);
 })
 
 export default api;
