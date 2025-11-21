bda3a5d093de2bae31cf43546ca764574774da21
NathanWSL <nathaniel.christian2077@gmail.com>
2025-11-21 13:59:44 -0300

    Migration to introduce ImageUrl

---
+++ backend/prisma/migrations/20251120235346_add_image_url_to_entities/migration.sql
@@ -0,0 +1,14 @@
+-- AlterTable
+ALTER TABLE "public"."Campaign" ADD COLUMN     "imageUrl" TEXT;
+
+-- AlterTable
+ALTER TABLE "public"."Character" ADD COLUMN     "imageUrl" TEXT;
+
+-- AlterTable
+ALTER TABLE "public"."Event" ADD COLUMN     "imageUrl" TEXT;
+
+-- AlterTable
+ALTER TABLE "public"."Location" ADD COLUMN     "imageUrl" TEXT;
+
+-- AlterTable
+ALTER TABLE "public"."ObjectModel" ADD COLUMN     "imageUrl" TEXT;
