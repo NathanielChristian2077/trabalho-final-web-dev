7533038275b392733d98c6dd12ac569c774ee611
NathanWSL <nathaniel.christian2077@gmail.com>
2025-11-26 22:07:41 -0300

    updated compiler options

--- frontend/src/app/providers.tsx
+++ frontend/src/app/providers.tsx
@@ -1,11 +1,17 @@
+import { ReactNode } from "react";
 import { RouterProvider } from "react-router-dom";
 import { ToastProvider } from "../components/layout/ToastProvider";
 import { router } from "./router";
 
-export function AppProviders() {
+type AppProvidersProps = {
+  children: ReactNode;
+};
+
+export function AppProviders({ children }: AppProvidersProps) {
   return (
     <ToastProvider>
       <RouterProvider router={router} />
+      {children}
     </ToastProvider>
   );
 }
